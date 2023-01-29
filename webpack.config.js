3390c2c061a0d051150d500e96f096431c1d819c
alialahverdi <alialahverdi9227@gmail.com>
2023-01-29 20:12:45 +0330

    install react and react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-    entry: path.resolve(__dirname, "src/index.js"),
+    entry: path.resolve(__dirname, "src/index.ts"),
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
