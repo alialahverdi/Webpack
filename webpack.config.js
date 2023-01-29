380ed9a3e2d126396ce1cfbe44dba891d285ead2
alialahverdi <alialahverdi9227@gmail.com>
2023-01-29 20:05:30 +0330

    added all config typescript like preset into babelrc and install babel preset typescript

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
         filename: "bundle.js"
     },
     resolve: {
+        /** "extensions" 
+         * If multiple files share the same name but have different extensions, webpack will 
+         * resolve the one with the extension listed first in the array and skip the rest. 
+         * This is what enables users to leave off the extension when importing
+         */
         extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
     },
     plugins: [
@@ -22,16 +27,17 @@ module.exports = {
         new MiniCssExtractPlugin(),
     ],
     module: {
+        /** "rules"
+         * This says - "Hey webpack compiler, when you come across a path that resolves to a '.js or .jsx' 
+         * file inside of a require()/import statement, use the babel-loader to transform it before you 
+         * add it to the bundle. And in this process, kindly make sure to exclude node_modules folder from 
+         * being searched"
+         */
         rules: [
             {
                 test: /\.(js|ts)x?$/i,
                 exclude: /node_module/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        preset: ["@babel/preset-env"]
-                    }
-                }
+                use: "babel-loader"
             },
             {
                 test: /\.css$/i,
